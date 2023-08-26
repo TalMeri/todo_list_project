a4844c3c93dd6ef02f5dfb396e8018d14cfc431b
TalMeri <tal.meridor@gmail.com>
2023-08-26 20:29:49 +0300

    add theme

--- src/App.js
+++ src/App.js
@@ -2,12 +2,31 @@ import React from "react";
 import './App.css';
 import Todolist from './components/Todolist';
 import Header from './components/Header';
+import { createTheme, ThemeProvider } from "@mui/material";
+
+const theme = createTheme({
+  palette: {
+    mode: 'light',
+    primary: {
+      main: '#663671',
+    },
+    secondary: {
+      main: '#d66d92',
+    },
+    success: {
+      main: '#66bb6a',
+      contrastText: 'rgba(255,255,255,0.87)',
+    },
+  },
+});
 
 function App() {
   return (
     <div>
+      <ThemeProvider theme={theme}>
       <Header></Header>
       <Todolist></Todolist>
+      </ThemeProvider>
     </div>
   );
 };
